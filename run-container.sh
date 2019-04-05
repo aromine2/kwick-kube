deb2f17ac0eca8f5f56f3667f9123f9d48f0fa52
Alex Romine <alex.romine@pioneer.com>
2019-04-05 12:48:04 -0500

    finalize container for now, delete .idea.

--- run-container.sh
+++ run-container.sh
@@ -1,3 +1,1 @@
-#docker build -t kwick-kube . && docker run -it --rm --entrypoint bash -v $PWD:/code kwick-kube
 docker build -t kwick-kube . && docker run -it --rm --entrypoint bash -v $PWD/kwick-kube:/kwick-kube kwick-kube
-
